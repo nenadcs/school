87e15ce1face2f7504d83cf70e47b660c098d71f
nenadcs <enemc92@hotmail.com>
2024-05-09 15:52:00 +0200

    gg

--- rust/code/hello_cargo/target/debug/deps/hello_cargo-e17c80ed162e690a.d
+++
@@ -1,5 +0,0 @@
-/home/nenadcs/Desktop/school/rust/code/hello_cargo/target/debug/deps/libhello_cargo-e17c80ed162e690a.rmeta: src/main.rs
-
-/home/nenadcs/Desktop/school/rust/code/hello_cargo/target/debug/deps/hello_cargo-e17c80ed162e690a.d: src/main.rs
-
-src/main.rs:
